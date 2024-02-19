9f5aa1dd6c10d5f84bc8f9903547c0534b877669
Romain Fallet <romain@fallet.net>
2024-02-19 15:21:48 +0100

    chore(tests): 🔧 add command to run unit tests

---
+++ src/application/index.spec.ts
@@ -0,0 +1,8 @@
+import { strictEqual } from 'node:assert'
+import { describe, it } from 'node:test'
+
+describe('test', () => {
+  it('should be true', () => {
+    strictEqual(true, true)
+  })
+})
